f21c0d0ab3759611e041b5a0669ef804c4172844
Johannes Maas <y0hy0h@gmx.net>
2020-06-12 18:25:40 +0200

    Work around problematic css-loader url resolution

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import "./style.css";
+// Disable url mangling.
+// Since our fonts are in `./static/fonts`, but will be published under `./fonts`,
+// we need to prevent webpack from trying to resolve the font's path,
+// because it will not find anything unter e.g. `./fonts/font-file.woff`.
+import "!style-loader!css-loader?url=false!./style.css";
 
 const { Elm } = require("./src/Main.elm");
 const pagesInit = require("elm-pages");
