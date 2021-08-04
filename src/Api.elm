bd95d2638ddc0f69828095c2a92001674da9010f
Y0hy0h <y0hy0h@gmx.net>
2021-08-04 18:08:54 +0200

    Add sitemap

--- src/Api.elm
+++ src/Api.elm
@@ -4,11 +4,30 @@ import ApiRoute
 import DataSource exposing (DataSource)
 import Html exposing (Html)
 import Route exposing (Route)
+import Sitemap
 
 
 routes :
     DataSource (List Route)
     -> (Html Never -> String)
     -> List (ApiRoute.ApiRoute ApiRoute.Response)
-routes _ _ =
-    []
+routes getStaticRoutes _ =
+    [ ApiRoute.succeed
+        (getStaticRoutes
+            |> DataSource.map
+                (\allRoutes ->
+                    { body =
+                        allRoutes
+                            |> List.map
+                                (\route ->
+                                    { path = Route.routeToPath route |> String.join "/"
+                                    , lastMod = Nothing
+                                    }
+                                )
+                            |> Sitemap.build { siteUrl = "https://elm-pages.com" }
+                    }
+                )
+        )
+        |> ApiRoute.literal "sitemap.xml"
+        |> ApiRoute.single
+    ]
